6717afa7d68c39944cd50b0a2e8d77fc9c02dedc
federico.manganiello <federico.manganiello@senseisrl.it>
2016-04-14 12:47:21 +0200

    aggiunti filtri

--- client/js/controllers/giorni.js
+++ client/js/controllers/giorni.js
@@ -2,23 +2,21 @@ angular
   .module('app')
   .controller('GiorniController', ['$scope', '$state', 'NgTableParams', '$resource', function($scope, 
 		  $state, NgTableParams, $resource) {
-    console.log('inside GiorniController...');
-      
+	var ref = this;
+    console.log('inside GiorniController...');      
     var query = $resource('http://localhost:3000/query_giorni_lav_mese');
-    console.log('query' + query);
-    this.tableParams = new NgTableParams({}, {
-		getData: function(params) {
-			console.log('params: ' + params);
-			// ajax request to query
-			return query.get().$promise.then(function(data) {
-				console.log('data: ' + data);
-					
-				
-				//params.total(data.inlineCount); // recal. page nav controls
-				console.log('data.results: ' + data.results);
-				return data.results;				
-			});
-		}
-    });    
-    
+    var queryData = {
+    	callFun : function (){     	    
+        	query.get().$promise.then(function(data) {    
+    			if (data!=null & data.results.length>0) {
+    				console.log('data.results: ' + data.results);
+	    			var res = data.results;
+	    			ref.tableParams = new NgTableParams({}, {
+	    				data: res
+	    			});
+    			}
+        	});
+    	}
+    };
+    queryData['callFun']();    
   }]);
--- client/js/controllers/oremese.js
+++ client/js/controllers/oremese.js
@@ -2,49 +2,21 @@ angular
   .module('app')
   .controller('OreMeseController', ['$scope', '$state', 'NgTableParams', '$resource', function($scope, 
 		  $state, NgTableParams, $resource) {
-    console.log('inside OreMeseController...');
-      
-    var query = $resource('http://localhost:3000/query_ore_lav_utente_mese');
-    console.log('query' + query);
-    alert('ciao');
-    
-    var resList = [];
+	var ref = this;
+    console.log('inside OreMeseController ...');      
+    var query = $resource('http://localhost:3000/query_ore_lav_utente_mese');            
     var queryData = {
     	callFun : function (){     	    
-		        	return query.get().$promise.then(function(data) {    
-		    			console.log('data: ' + data);
-		    			
-		    			
-		    			//params.total(data.inlineCount); // recal. page nav controls
-		    			console.log('data.results: ' + data.results);
-		    			
-		    			var res = [{"anno": 2016, "mese": 3, "nomeDipendente": "Viani", "oreMese": 64},
-		    			               {"anno": 2016, "mese": 3, "nomeDipendente": "Terranova", "oreMese": 56}];
-		    	//		return data.results;	
-		    			return res;
-		        	});
+        	query.get().$promise.then(function(data) {    
+    			if (data!=null & data.results.length>0) {
+    				console.log('data.results: ' + data.results);
+	    			var res = data.results;
+	    			ref.tableParams = new NgTableParams({}, {
+	    				data: res
+	    			});
+    			}
+        	});
     	}
     };
-    resList = queryData['callFun']();
-    console.log('resList' + resList);
-//    var resList = [{"anno": 2016, "mese": 3, "nomeDipendente": "Viani", "oreMese": 64},
-//               {"anno": 2016, "mese": 3, "nomeDipendente": "Terranova", "oreMese": 56}];
-//    console.log('resList' + resList);
-    
-    this.tableParams = new NgTableParams({}, {
-    	data: resList
-//		getData: function(params) {
-//			console.log('params: ' + params);
-//			// ajax request to query
-//			return query.get().$promise.then(function(data) {
-//				console.log('data: ' + data);
-//					
-//				
-//				//params.total(data.inlineCount); // recal. page nav controls
-//				console.log('data.results: ' + data.results);
-//				return data.results;				
-//			});
-//		}
-    });    
-    
+    queryData['callFun']();    
   }]);
