3bd7090699085c375f583b6508bc40083e29bdb4
federico.manganiello <federico.manganiello@senseisrl.it>
2016-04-27 11:48:07 +0200

    finito il metodo di ripartizione del budget

--- client/js/controllers/calendar.js
+++ client/js/controllers/calendar.js
@@ -24,19 +24,60 @@ angular
 	    $scope.getBudgets = function(budgettot, dayfrom, dayto) {
 	    	$scope.budgets = [];
 	    	console.log('budgettot: ' + budgettot);
-	    	console.log('dayfrom: ' + dayfrom);
-	    	console.log('dayto: ' + dayto);
-	    	console.log('dayfrom month: ' + dayfrom.month());
-	    	console.log('dayto month: ' + dayto.month());
-	    	for (var i = dayfrom.month(); i <= dayto.month(); i++) {
-	    		console.log('month: ' + moment.months()[i]);
-	    		console.log('date: ' + moment({month: i}).date());
-	    		
-	    		var budget = {
-	    			month: moment.months()[i],
-	    			days: ''
-	    		};
-	    		$scope.budgets.push(budget);
+	    	console.log('diff in months: ' + dayto.diff(dayfrom, 'months'));	    	
+	    	
+	    	if (dayfrom.isBefore(dayto)) {
+	    		var totaldays = dayto.diff(dayfrom, 'days') + 1;
+	    		console.log('totaldays: ' + totaldays);
+	    		if (dayto.diff(dayfrom, 'months') === 0) {
+	    			var budgettot = {
+    	    			month: moment.months()[dayfrom.month()],
+    	    			days: dayfrom.daysInMonth() - dayfrom.date() + 1,
+    	    			amount: budgettot
+    	    		};
+	    			$scope.budgets.push(budgettot);
+	    		} else if (dayto.diff(dayfrom, 'months') === 1) {
+	    			var budgetFrom = {
+    	    			month: moment.months()[dayfrom.month()],
+    	    			days: dayfrom.daysInMonth() - dayfrom.date() + 1,
+    	    			amount: budgettot * ((dayfrom.daysInMonth() - dayfrom.date() + 1)/totaldays)
+    	    		};
+	    			$scope.budgets.push(budgetFrom);
+	    			
+	    			var budgetTo = {
+    	    			month: moment.months()[dayto.month()],
+    	    			days: dayto.date(),
+    	    			amount: budgettot * (dayto.date()/totaldays)
+    	    		};	    			
+	    			$scope.budgets.push(budgetTo);
+	    			console.log('budgets: ' + JSON.stringify($scope.budgets));
+	    		} else {
+	    			var budgetFrom = {
+    	    			month: moment.months()[dayfrom.month()],
+    	    			days: dayfrom.daysInMonth() - dayfrom.date() + 1,
+    	    			amount: budgettot * ((dayfrom.daysInMonth() - dayfrom.date() + 1)/totaldays)
+    	    		};
+	    			$scope.budgets.push(budgetFrom);
+	    			
+	    			for (var i = dayfrom.month() + 1; i < dayto.month(); i++) {
+	    	    		var budget = {
+	    	    			month: moment.months()[i],
+	    	    			days: moment({month: i}).daysInMonth(),
+	    	    			amount: budgettot * (moment({month: i}).daysInMonth()/totaldays)
+	    	    		};
+	    	    		$scope.budgets.push(budget);
+	    	    	}
+	    			
+	    			var budgetTo = {
+    	    			month: moment.months()[dayto.month()],
+    	    			days: dayto.date(),
+    	    			amount: budgettot * (dayto.date()/totaldays)
+    	    		};	    			
+	    			$scope.budgets.push(budgetTo);
+	    		}	    		
+	    	} else {
+	    		throw 'data di inizio maggiore di quella finale';
 	    	}
+	    	
 	    };
 	});
