040d286bbc2e612edfd81feadebcd15d31c3c0cc
federico.manganiello <federico.manganiello@senseisrl.it>
2016-06-12 20:04:04 +0200

    sistemato file di generazione gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,41 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-//var uglify = require('gulp-uglify');
-//var runSequence = require('run-sequence');
+var runSequence = require('run-sequence');
 var del = require('del');
-//var cache = require('gulp-cache');
 var config = require('./gulpconfig.json');
-
-var paths = {
-	build : []
-};
-
-function doStuff(cfg) {
-	gutil.log('configuration = ', cfg, gutil.colors.magenta('123'));
-	return gulp.src(cfg.src)
-//		.pipe(uglify())
-		.pipe(gulp.dest(cfg.dest));
-}
 
 // Clean build folder function:
 function cleanBuildFn() {
-    return del.sync(paths.build);
+	gutil.log('cleaning build folder...');
+    return del.sync([config.local.dest, config.test.dest]);
 }
 
-// Build function:
-function buildFn(cb) {
-//    runSequence(
-//        'clean:build', // run synchronously first
-//        ['scripts, 'images'], // then run rest asynchronously
-//        'watch',
-//        cb
-//    );
+//Build local
+function buildLocalFn() {
+	return gulp.src(config.local.src)
+    .pipe(gulp.dest(config.local.dest));
 }
 
-// Scripts function:
-function scriptsFn() {
-//  return gulp.src(paths.scripts)
-////    .pipe(coffee())
-////    .pipe(uglify())
-////    .pipe(concat('all.min.js'))
-//    .pipe(gulp.dest('build/js'));
+//Build test
+function buildTestFn() {
+	return gulp.src(config.test.src)
+    .pipe(gulp.dest(config.test.dest));
 }
 
-// Images function with caching added:
-function imagesFn() {
-//    gulp.src(paths.source + '/images/**/*.+(png|jpg|gif|svg)')
-//    .pipe(cache(imagemin({optimizationLevel: 5})))
-//    .pipe(gulp.dest(paths.build + '/images'));
-}
-
-// Clean tasks:
-gulp.task('clean:build', cleanBuildFn);
-
-// Scripts task:
-gulp.task('scripts', scriptsFn);
-
-// Images task:
-gulp.task('images', imagesFn);
-
-// Watch for changes on files:
-gulp.task('watch', function() {
-//    gulp.watch(paths.source + '/images/**/*.+(png|jpg|gif|svg)', ['images']);
-//    gulp.watch(paths.source + '/scripts/**/*.js', ['scripts']);
+gulp.task('clean:build', function() {
+	runSequence('clean',
+				['build:local', 'build:test']);
 });
 
-// Default task:
-//gulp.task('default', buildFn);
+// Clean tasks:
+gulp.task('clean', cleanBuildFn);
+
+//Build tasks:
+gulp.task('build:local', buildLocalFn);
+gulp.task('build:test', buildTestFn);
+
+// Default task: Check configuration
 gulp.task('default', function() {
-	doStuff(config.local);
-	doStuff(config.test);
+	gutil.log('checking configuration...');
+	gutil.log('config: ' + JSON.stringify(config, null, '\t'));
 });
