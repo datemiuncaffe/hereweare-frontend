943c95adca104f881413e898b8fef99807c0b25f
federico.manganiello <federico.manganiello@senseisrl.it>
2016-06-17 14:53:18 +0200

    aggiunto click datepicker sull icona calendario

--- client/js/controllers/estimate/projectmodify.js
+++ client/js/controllers/estimate/projectmodify.js
@@ -1,12 +1,12 @@
 angular
 	.module("app")
-	.controller("ProjectModifyController", ['$scope', '$resource', '$q', '$stateParams', 'crud', 'resourceBaseUrl', 
+	.controller("ProjectModifyController", ['$scope', '$resource', '$q', '$stateParams', 'crud', 'resourceBaseUrl',
 	                                        function($scope, $resource, $q, $stateParams, crud, resourceBaseUrl) {
-		/* entities */	    
+		/* entities */
 	    $scope.customer = {
 	    	name: null
 	    };
-	    
+
 	    $scope.project = {
 	    	name: null,
 	    	code: null,
@@ -16,11 +16,11 @@ angular
 	    	daystot: null,
 	    	customerId: null,
 	    	budgets: []
-	    };	    
+	    };
 	    /* end entities */
-	    
+
 	    /* loading project */
-	    if ($stateParams != null && 
+	    if ($stateParams != null &&
 	    	$stateParams.code != null && $stateParams.code.length > 0 &&
 	    	$stateParams.customer != null && $stateParams.customer.length > 0) {
 	    	console.log('Project code: ' + $stateParams.code + '; customer: ' + decodeURI($stateParams.customer));
@@ -28,14 +28,14 @@ angular
 	    	var projectRes = $resource(queryUrl, null, {'query':  {method:'GET', isArray:true}});
 	    	projectRes.query().$promise.then(function(data) {
 				console.log('project: ' + JSON.stringify(data[0]));
-				
+
 				$scope.customer = JSON.parse(decodeURI($stateParams.customer));
 				$scope.project = data[0];
-							
+
 			});
 	    }
 	    /* end loading project */
-	    
+
 	    /* datepickers */
 		var datepickerfrom = new Pikaday({
 			field : document.getElementById('datepickerfrom'),
@@ -51,8 +51,10 @@ angular
 			maxDate : new Date(2020, 12, 31),
 			yearRange : [ 2000, 2020 ]
 		});
+		$scope.datepickerfrom = datepickerfrom;
+		$scope.datepickerto = datepickerto;
 		/* end datepickers */
-	    
+
 	    $scope.getBudgets = function(budgettot, daystot, selectedfrom, selectedto) {
 	    	if (budgettot == null) {
 	    		throw 'ERR: il budget totale non è stato inserito';
@@ -67,11 +69,11 @@ angular
 	    	if (selectedto == null) {
 	    		throw 'ERR: inserire la data finale';
 	    	}
-	    	
+
 	    	var from = moment(selectedfrom, "YYYY-MM-DD");
-	    	var to = moment(selectedto, "YYYY-MM-DD");	    	
-	    	console.log('diff in months: ' + to.diff(from, 'months'));	    	
-	    	
+	    	var to = moment(selectedto, "YYYY-MM-DD");
+	    	console.log('diff in months: ' + to.diff(from, 'months'));
+
 	    	if (from.isBefore(to)) {
 	    		$scope.project.budgets = [];
 	    		var totaldays = to.diff(from, 'days') + 1;
@@ -98,7 +100,7 @@ angular
     	    			projectId: $scope.project.id
     	    		};
 	    			$scope.project.budgets.push(budgetFrom);
-	    			
+
 	    			var budgetTo = {
 	    				from: to.year() + '-' + to.month() + '-01',
 		    			to: to.year() + '-' + to.month() + '-' + to.date(),
@@ -107,7 +109,7 @@ angular
     	    			days: parseFloat((daystot * (to.date()/totaldays)).toFixed(2)),
     	    			amount: parseFloat((budgettot * (to.date()/totaldays)).toFixed(2)),
     	    			projectId: $scope.project.id
-    	    		};	    			
+    	    		};
 	    			$scope.project.budgets.push(budgetTo);
 	    			console.log('budgets: ' + JSON.stringify($scope.project.budgets));
 	    		} else {
@@ -121,7 +123,7 @@ angular
     	    			projectId: $scope.project.id
     	    		};
 	    			$scope.project.budgets.push(budgetFrom);
-	    			
+
 	    			for (var i = from.month() + 1; i < to.month(); i++) {
 	    	    		var budget = {
 	    	    			from: moment({month: i}).year() + '-' + i + '-' + moment({month: i}).date(),
@@ -134,7 +136,7 @@ angular
 	    	    		};
 	    	    		$scope.project.budgets.push(budget);
 	    	    	}
-	    			
+
 	    			var budgetTo = {
 	    				from: to.year() + '-' + to.month() + '-01',
 			    		to: to.year() + '-' + to.month() + '-' + to.date(),
@@ -143,25 +145,25 @@ angular
     	    			days: parseFloat((daystot * (to.date()/totaldays)).toFixed(2)),
     	    			amount: parseFloat((budgettot * (to.date()/totaldays)).toFixed(2)),
     	    			projectId: $scope.project.id
-    	    		};	    			
+    	    		};
 	    			$scope.project.budgets.push(budgetTo);
 	    		}
 	    		for (var i in $scope.project.budgets) {
 	    			console.log('budget amount: ' + $scope.project.budgets[i].amount + '; type: ' + typeof $scope.project.budgets[i].amount);
 	    		}
-	    		
+
 	    	} else {
 	    		throw 'data di inizio maggiore di quella finale';
-	    	}	    	
+	    	}
 	    };
-	    
+
 	    var updateproject = $resource('http://' + resourceBaseUrl + '/api/projects', null, {'update': {method:'PUT'}});
 	    var updatebudgets = $resource('http://' + resourceBaseUrl + '/api/budgets/updateAllByProjectId', null, {'update': {method:'PUT'}});
 
 	    $scope.save = function() {
 			console.log('current customer: ' + JSON.stringify($scope.customer));
-			console.log('updating project: ' + JSON.stringify($scope.project));			
-			
+			console.log('updating project: ' + JSON.stringify($scope.project));
+
 			$scope.project.customerId = $scope.customer.id;
 			updateproject.update($scope.project).$promise.then(function(updatedproject) {
 				console.log('updatedproject: ' + JSON.stringify(updatedproject));
@@ -169,7 +171,7 @@ angular
 					console.log('updatedbudgets: ' + JSON.stringify(updatedbudgets));
 				});
 			});
-						
+
 		};
-			    
+
 	}]);
