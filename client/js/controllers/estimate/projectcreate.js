b5aedea600c53d05841d4fdb2cd56d926280234a
federico.manganiello <federico.manganiello@senseisrl.it>
2016-06-27 15:14:06 +0200

    cambiata procedura calcolo budget

--- client/js/controllers/estimate/projectcreate.js
+++ client/js/controllers/estimate/projectcreate.js
@@ -38,14 +38,105 @@ angular
 			$scope.datepickerto = datepickerto;
 			/* end datepickers */
 
-	    $scope.getBudgets = function(budgettot, daystot, selectedfrom, selectedto) {
+			var holidays = [
+				{date: "XXXX-01-01",description: "capodanno"},
+				{date: "XXXX-01-06",description: "epifania"},
+				{date: "XXXX-04-25",description: "liberazione"},
+				{date: "XXXX-05-01",description: "festa del lavoro"},
+				{date: "XXXX-06-02",description: "festa della repubblica"},
+				{date: "XXXX-08-15",description: "ferragosto"},
+				{date: "XXXX-11-01",description: "ognissanti"},
+				{date: "XXXX-11-02",description: "festa dei morti"},
+				{date: "XXXX-12-08",description: "immacolata"},
+				{date: "XXXX-12-25",description: "natale"},
+				{date: "XXXX-12-26",description: "santo stefano"}
+			];
+
+			function isholiday(month, dayofmonth) {
+				var isholiday = false;
+				var zero2 = new Padder(2);
+				var date = "XXXX-" + zero2.pad(month + 1) + "-" + zero2.pad(dayofmonth);
+				isholiday = holidays.map(function(o) {
+					return o.date;
+				}).includes(date);
+				console.log("date " + date + " isholiday " + isholiday);
+				return isholiday;
+			}
+
+			function getBusinessDays(from, to) {
+				var daystart = from.clone();
+				var dayend = to.clone();
+
+				var monthsmap = new Map();
+				var weeksmap = null;
+				var month = 0;
+				var week = 0;
+				var days = [];
+				while(dayend.diff(daystart, 'days') >= 0) {
+					month = daystart.month();
+					console.log('month: ' + month);
+					if (monthsmap.has(month)) {
+						weeksmap = monthsmap.get(month);
+						week = daystart.week();
+						if (weeksmap.has(week)) {
+							days = weeksmap.get(week);
+							if (daystart.day() != 0 && daystart.day() != 6 &&
+									!isholiday(month, daystart.date())) {
+								days.push(daystart.date());
+							}
+							console.log('days: ' + days);
+						} else {
+							console.log('new week of month: ' + month);
+							days = [];
+							if (daystart.day() != 0 && daystart.day() != 6 &&
+									!isholiday(month, daystart.date())) {
+								days.push(daystart.date());
+							}
+							weeksmap.set(week, days);
+							console.log('days: ' + days);
+						}
+					} else {
+						console.log('new month');
+						weeksmap = new Map();
+						week = daystart.week();
+						days = [];
+						if (daystart.day() != 0 && daystart.day() != 6 &&
+								!isholiday(month, daystart.date())) {
+							days.push(daystart.date());
+						}
+						weeksmap.set(week, days);
+						monthsmap.set(month, weeksmap);
+						console.log('days: ' + days);
+					}
+					daystart.add(1, 'day');
+				}
+				console.log('monthsmap size: ' + monthsmap.size);
+
+				return monthsmap;
+			};
+
+			function getDaysTot(monthsmap) {
+				var daysTot = 0;
+				var months = Array.from(monthsmap.keys());
+				console.log('months: ' + months);
+				months.forEach(function(month){
+					var weeksmap = monthsmap.get(month);
+					var weeks = Array.from(weeksmap.keys());
+					weeks.forEach(function(week){
+						var days = weeksmap.get(week);
+						console.log('m[' + month + '-' + week + '] = ' + JSON.stringify(days));
+						daysTot += days.length;
+					});
+				});
+				return daysTot;
+			}
+
+	    $scope.getBudgets = function(budgettot, selectedfrom, selectedto) {
 	    	if (budgettot == null) {
 	    		throw 'ERR: il budget totale non è stato inserito';
 	    	}
 	    	console.log('budgettot: ' + budgettot);
-	    	if (daystot == null) {
-	    		throw 'ERR: i giorni totali non sono stati inseriti';
-	    	}
+
 	    	if (selectedfrom == null) {
 	    		throw 'ERR: inserire la data iniziale';
 	    	}
@@ -59,72 +150,45 @@ angular
 
 	    	if (from.isBefore(to)) {
 	    		$scope.project.budgets = [];
-	    		var totaldays = to.diff(from, 'days') + 1;
-	    		console.log('totaldays: ' + totaldays);
-	    		if (to.diff(from, 'months') === 0) {
-	    			var budgettot = {
-	    					from: from.year() + '-' + from.month() + '-' + from.date(),
-	    					to: from.year() + '-' + from.month() + '-' + from.daysInMonth(),
-	    					year: from.year(),
-    	    			month: moment.months()[from.month()],
-    	    			days: daystot,
-    	    			amount: budgettot
-    	    		};
-	    			$scope.project.budgets.push(budgettot);
-	    		} else if (to.diff(from, 'months') === 1) {
-	    			var budgetFrom = {
-	    					from: from.year() + '-' + from.month() + '-' + from.date(),
-		    				to: from.year() + '-' + from.month() + '-' + from.daysInMonth(),
-		    				year: from.year(),
-    	    			month: moment.months()[from.month()],
-    	    			days: parseFloat((daystot * ((from.daysInMonth() - from.date() + 1)/totaldays)).toFixed(2)),
-    	    			amount: parseFloat((budgettot * ((from.daysInMonth() - from.date() + 1)/totaldays)).toFixed(2))
-    	    		};
-	    			$scope.project.budgets.push(budgetFrom);
+					var cyear = from.year();
 
-	    			var budgetTo = {
-	    					from: to.year() + '-' + to.month() + '-01',
-		    				to: to.year() + '-' + to.month() + '-' + to.date(),
-		    				year: to.year(),
-    	    			month: moment.months()[to.month()],
-    	    			days: parseFloat((daystot * (to.date()/totaldays)).toFixed(2)),
-    	    			amount: parseFloat((budgettot * (to.date()/totaldays)).toFixed(2))
-    	    		};
-	    			$scope.project.budgets.push(budgetTo);
-	    			console.log('budgets: ' + JSON.stringify($scope.project.budgets));
-	    		} else {
-	    			var budgetFrom = {
-		    				from: from.year() + '-' + from.month() + '-' + from.date(),
-			    			to: from.year() + '-' + from.month() + '-' + from.daysInMonth(),
-			    			year: from.year(),
-	    	    		month: moment.months()[from.month()],
-	    	    		days: parseFloat((daystot * ((from.daysInMonth() - from.date() + 1)/totaldays)).toFixed(2)),
-	    	    		amount: parseFloat((budgettot * ((from.daysInMonth() - from.date() + 1)/totaldays)).toFixed(2))
-    	    		};
-	    			$scope.project.budgets.push(budgetFrom);
+					var bdays = getBusinessDays(from, to);
+					var daystot = getDaysTot(bdays);
+					$scope.project.daystot = daystot;
+					console.log('daystot: ' + $scope.project.daystot);
 
-	    			for (var i = from.month() + 1; i < to.month(); i++) {
-	    	    		var budget = {
-	    	    			from: moment({month: i}).year() + '-' + i + '-' + moment({month: i}).date(),
-	    			    	to: moment({month: i}).year() + '-' + i + '-' + moment({month: i}).daysInMonth(),
-	    			    	year: moment({month: i}).year(),
-	    	    			month: moment.months()[i],
-	    	    			days: parseFloat((daystot * (moment({month: i}).daysInMonth()/totaldays)).toFixed(2)),
-	    	    			amount: parseFloat((budgettot * (moment({month: i}).daysInMonth()/totaldays)).toFixed(2))
-	    	    		};
-	    	    		$scope.project.budgets.push(budget);
-	    	    	}
+					var months = Array.from(bdays.keys());
+					months.forEach(function(cmonth){
+						var weeksmap = bdays.get(cmonth);
+						var weeks = Array.from(weeksmap.keys());
+						var firstWeek = weeks[0];
+						var lastWeek = weeks[weeks.length - 1];
+						var firstDay = weeksmap.get(firstWeek)[0];
+						var lastDay = null;
+						if (weeksmap.get(lastWeek).length > 0) {
+							lastDay = weeksmap.get(lastWeek)[weeksmap.get(lastWeek).length - 1];
+						} else {
+							lastDay = weeksmap.get(lastWeek - 1)[weeksmap.get(lastWeek - 1).length - 1];
+						}
 
-	    			var budgetTo = {
-		    				from: to.year() + '-' + to.month() + '-01',
-				    		to: to.year() + '-' + to.month() + '-' + to.date(),
-				    		year: to.year(),
-	    	    		month: moment.months()[to.month()],
-	    	    		days: parseFloat((daystot * (to.date()/totaldays)).toFixed(2)),
-	    	    		amount: parseFloat((budgettot * (to.date()/totaldays)).toFixed(2))
-    	    		};
-	    			$scope.project.budgets.push(budgetTo);
-	    		}
+						var monthdays = 0;
+						weeks.forEach(function(week){
+							var days = weeksmap.get(week);
+							monthdays += days.length;
+						});
+
+						var budget = {
+	    				from: cyear + '-' + cmonth + '-' + firstDay,
+	    				to: cyear + '-' + cmonth + '-' + lastDay,
+	    				year: cyear,
+    	    		month: moment.months()[cmonth],
+							days: monthdays,
+							amount: parseFloat((budgettot * (monthdays/daystot)).toFixed(2)),
+    	    		projectId: $scope.project.id
+    	    	};
+	    			$scope.project.budgets.push(budget);
+					});
+
 	    		for (var i in $scope.project.budgets) {
 	    			console.log('budget amount: ' + $scope.project.budgets[i].amount + '; type: ' + typeof $scope.project.budgets[i].amount);
 	    		}
@@ -149,5 +213,23 @@ angular
 					});
 				});
 			};
+
+			function Padder(len, pad) {
+				if (len === undefined) {
+					len = 1;
+				} else if (pad === undefined) {
+					pad = '0';
+				}
+
+				var pads = '';
+				while (pads.length < len) {
+					pads += pad;
+				}
+
+				this.pad = function(what) {
+					var s = what.toString();
+					return pads.substring(0, pads.length - s.length) + s;
+				};
+			}
 
 	}]);
