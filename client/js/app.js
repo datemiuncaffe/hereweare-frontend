24fc61531ca3b7d6f4a87bde1542fb0e4787d8eb
federico.manganiello <federico.manganiello@senseisrl.it>
2016-07-11 17:39:16 +0200

    menu di navigazione verticale

--- client/js/app.js
+++ client/js/app.js
@@ -6,7 +6,11 @@ angular
     'ui.router',
     'ngTable',
     'angucomplete-alt',
-    'crudService'
+    'crudService',
+    'myMenuApp.controllers',
+    'ngAnimate',
+    'ngMaterial',
+    'ngAria'
   ])
   .config(['$stateProvider', '$urlRouterProvider', '$provide', function($stateProvider, $urlRouterProvider, $provide) {
       $stateProvider
@@ -37,15 +41,6 @@ angular
            }
         }
       })
-      .state('projectcreate', {
-        url: '/projectcreate',
-        views:{
-          'projectcreateview': {
-              templateUrl: 'views/estimate/projectcreate.html',
-              controller: 'ProjectCreateController'
-           }
-        }
-      })
       .state('projectdetail', {
         url: '/projectdetail?customerId&customerName&projectId&projectName&projectCode',
         views:{
@@ -65,61 +60,80 @@ angular
         }
       })
       .state('oremese', {
-		url: '/oremese',
-		views:{
-		  'oremeseview': {
-	         templateUrl: 'views/ehourqueries/oremese.html',
-	         controller: 'OreMeseController'
-		  }
-		}
+    		url: '/oremese',
+    		views:{
+    		  'oremeseview': {
+    	         templateUrl: 'views/ehourqueries/oremese.html',
+    	         controller: 'OreMeseController'
+    		  }
+    		}
       })
       .state('giornicommessautente', {
-		url: '/giornicommessautente',
-		views:{
-		  'giornicommessautenteview': {
-	         templateUrl: 'views/ehourqueries/giornicommessautente.html',
-	         controller: 'GiorniCommessaUtenteController'
-		  }
-		}
+    		url: '/giornicommessautente',
+    		views:{
+    		  'giornicommessautenteview': {
+    	         templateUrl: 'views/ehourqueries/giornicommessautente.html',
+    	         controller: 'GiorniCommessaUtenteController'
+    		  }
+    		}
       })
       .state('giornicommessa', {
-		url: '/giornicommessa',
-		views:{
-		  'giornicommessaview': {
-	         templateUrl: 'views/ehourqueries/giornicommessa.html',
-	         controller: 'GiorniCommessaController'
-		  }
-		}
+    		url: '/giornicommessa',
+    		views:{
+    		  'giornicommessaview': {
+    	         templateUrl: 'views/ehourqueries/giornicommessa.html',
+    	         controller: 'GiorniCommessaController'
+    		  }
+    		}
       })
       .state('giorni', {
-		url: '/giorni',
-		views:{
-		  'giorniview': {
-	         templateUrl: 'views/ehourqueries/giorni.html',
-	         controller: 'GiorniController'
-		  }
-		}
+    		url: '/giorni',
+    		views:{
+    		  'giorniview': {
+    	         templateUrl: 'views/ehourqueries/giorni.html',
+    	         controller: 'GiorniController'
+    		  }
+    		}
       })
       .state('giornicliente', {
-		url: '/giornicliente',
-		views:{
-		  'giorniclienteview': {
-	         templateUrl: 'views/ehourqueries/giornicliente.html',
-	         controller: 'GiorniClienteController'
-		  }
-		}
+    		url: '/giornicliente',
+    		views:{
+    		  'giorniclienteview': {
+    	         templateUrl: 'views/ehourqueries/giornicliente.html',
+    	         controller: 'GiorniClienteController'
+    		  }
+    		}
       })
       .state('giorniclienteprogetto', {
-		url: '/giorniclienteprogetto',
-		views:{
-		  'giorniclienteprogettoview': {
-	         templateUrl: 'views/ehourqueries/giorniclienteprogetto.html',
-	         controller: 'GiorniClienteProgettoController'
-		  }
-		}
+    		url: '/giorniclienteprogetto',
+    		views:{
+    		  'giorniclienteprogettoview': {
+    	         templateUrl: 'views/ehourqueries/giorniclienteprogetto.html',
+    	         controller: 'GiorniClienteProgettoController'
+    		  }
+    		}
       });
 
       $urlRouterProvider.otherwise('overview');
 
       $provide.value('resourceBaseUrl', '$resourceBaseUrl$');
-  }]);
+  }])
+  //take all whitespace out of string
+  .filter('nospace', function () {
+    return function (value) {
+      return (!value) ? '' : value.replace(/ /g, '');
+    };
+  })
+  //replace uppercase to regular case
+  .filter('humanizeDoc', function () {
+    return function (doc) {
+      if (!doc) return;
+      if (doc.type === 'directive') {
+        return doc.name.replace(/([A-Z])/g, function ($1) {
+          return '-' + $1.toLowerCase();
+        });
+      }
+
+      return doc.label || doc.name;
+    };
+  });
---
+++ client/js/services/menu.service.js
@@ -0,0 +1,82 @@
+(function(){
+
+  'use strict';
+
+  angular.module('common.services')
+    .factory('menu', [
+      '$location',
+      '$rootScope',
+      function ($location) {
+
+        var sections = [{
+          name: 'Progetti attivi',
+          type: 'link',
+          state: 'activeprojects'
+        }, {
+          name: 'Overview',
+          type: 'link',
+          state: 'overview'
+        }, {
+          name: 'Ricerca',
+          type: 'link',
+          state: 'ricerca'
+        }];
+
+        sections.push({
+          name: 'Riepiloghi ore',
+          type: 'toggle',
+          pages: [{
+            name: 'Ore mese',
+            type: 'link',
+            state: 'oremese',
+            icon: ''
+          }, {
+            name: 'Giorni commessa utente',
+            type: 'link',
+            state: 'giornicommessautente',
+            icon: ''
+          }, {
+            name: 'Giorni',
+            type: 'link',
+            state: 'giorni',
+            icon: ''
+          }, {
+            name: 'Giorni cliente',
+            type: 'link',
+            state: 'giornicliente',
+            icon: ''
+          }, {
+            name: 'Giorni cliente progetto',
+            type: 'link',
+            state: 'giorniclienteprogetto',
+            icon: ''
+          }]
+        });
+
+        var self;
+
+        return self = {
+          sections: sections,
+
+          toggleSelectSection: function (section) {
+            self.openedSection = (self.openedSection === section ? null : section);
+          },
+          isSectionSelected: function (section) {
+            return self.openedSection === section;
+          },
+
+          selectPage: function (section, page) {
+            page && page.url && $location.path(page.url);
+            self.currentSection = section;
+            self.currentPage = page;
+          }
+        };
+
+        function sortByHumanName(a, b) {
+          return (a.humanName < b.humanName) ? -1 :
+            (a.humanName > b.humanName) ? 1 : 0;
+        }
+
+      }])
+
+})();
