700b7d2dfd1d86a0242e3361edae7b1d951a5579
federico.manganiello <federico.manganiello@senseisrl.it>
2016-09-28 10:45:36 +0200

    popolate le altre 2 select in pagina report dipendente

--- client/js/controllers/reporting/employee.js
+++ client/js/controllers/reporting/employee.js
@@ -25,8 +25,12 @@ angular
 				var employees = data[0];
 				$scope.employees = employees;
 				$scope.selectedEmployee = employees[0];
-				getReportWeeks($scope.reportIntervals.weeks);
+				getReportIntervals($scope.reportIntervals.weeks,
+													 $scope.reportIntervals.months,
+												 	 $scope.reportIntervals.quarters);
 				$scope.selectedWeek = $scope.reportIntervals.weeks[0];
+				$scope.selectedMonth = $scope.reportIntervals.months[0];
+				$scope.selectedQuarter = $scope.reportIntervals.quarters[0];
 				$scope.search($scope.selectedEmployee);
 			});
 
@@ -111,25 +115,66 @@ angular
 		  return table;
 		};
 
-		function getReportWeeks(reportWeeks) {
+		function getReportIntervals(reportWeeks,
+							reportMonths, reportQuarters) {
 			var currentDay = startDay;
 			var weeks = {};
+			var months = {};
+			var quarters = {};
 			while (currentDay.year() < endDay.year() ||
 						 (currentDay.week() <= endDay.week() &&
 						  currentDay.year() == endDay.year())) {
-				var numberOfWeek = currentDay.week() + '-' +
-							 currentDay.endOf('week').year();
-				var startOfWeek = currentDay.clone()
-						.startOf('week').format('YYYY-MM-DD');
-				var endOfWeek = currentDay.clone()
-						.endOf('week').format('YYYY-MM-DD');
+				var currentDayOfWeek = currentDay.clone();
+				var numberOfWeek = currentDayOfWeek.week() + '-' +
+						currentDayOfWeek.endOf('week').year();
+				var startOfWeek = currentDayOfWeek.startOf('week')
+						.format('YYYY-MM-DD');
+				var endOfWeek = currentDayOfWeek.endOf('week')
+						.format('YYYY-MM-DD');
 				var labelOfWeek = startOfWeek + ' | ' + endOfWeek;
+
 				weeks[numberOfWeek] = {
 					number: numberOfWeek,
 					start: startOfWeek,
 					end: endOfWeek,
 					label: labelOfWeek
 				};
+
+				var currentDayOfMonth = currentDay.clone();
+				var numberOfMonth = currentDayOfMonth.month() + '-' +
+						currentDayOfMonth.endOf('month').year();
+				var startOfMonth = currentDayOfMonth.startOf('month')
+						.format('YYYY-MM-DD');
+				var endOfMonth = currentDayOfMonth.endOf('month')
+						.format('YYYY-MM-DD');
+				var labelOfMonth = startOfMonth + ' | ' + endOfMonth;
+
+				if (months[numberOfMonth] == null) {
+					months[numberOfMonth] = {
+						number: numberOfMonth,
+						start: startOfMonth,
+						end: endOfMonth,
+						label: labelOfMonth
+					};
+				}
+
+				var currentDayOfQuarter = currentDay.clone();
+				var numberOfQuarter = currentDayOfQuarter.quarter() + '-' +
+						currentDayOfQuarter.endOf('quarter').year();
+				var startOfQuarter = currentDayOfQuarter.startOf('quarter')
+						.format('YYYY-MM-DD');
+				var endOfQuarter = currentDayOfQuarter.endOf('quarter')
+						.format('YYYY-MM-DD');
+				var labelOfQuarter = startOfQuarter + ' | ' + endOfQuarter;
+
+				if (quarters[numberOfQuarter] == null) {
+					quarters[numberOfQuarter] = {
+						number: numberOfQuarter,
+						start: startOfQuarter,
+						end: endOfQuarter,
+						label: labelOfQuarter
+					};
+				}
 				//console.log('currentDay: ' + currentDay.toString());
 				currentDay = currentDay.add(1, 'weeks');
 			}
@@ -138,6 +183,14 @@ angular
 			var weeksKeys = Object.keys(weeks);
 			weeksKeys.forEach(function(key) {
 				reportWeeks.push(weeks[key]);
+			});
+			var monthsKeys = Object.keys(months);
+			monthsKeys.forEach(function(key) {
+				reportMonths.push(months[key]);
+			});
+			var quartersKeys = Object.keys(quarters);
+			quartersKeys.forEach(function(key) {
+				reportQuarters.push(quarters[key]);
 			});
 		};
 
